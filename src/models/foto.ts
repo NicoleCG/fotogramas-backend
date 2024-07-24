0f331b8112a501d08a2160c4d8a3603c6534f328
Nicole Carvajal <nicole.carvajalg@gmail.com>
2024-07-23 21:20:08 -0400

    Modelo creado

---
+++ src/models/foto.ts
@@ -0,0 +1,15 @@
+import { Comentario } from "./comentario";
+import { Usuario } from "./usuario";
+
+export class Foto {
+    constructor(
+        public id: string,
+        public ruta: string,
+        public descripcion: string,
+        public hashtags: string[],
+        public fechaSubida: Date,
+        public usuario: Usuario,
+        public meGusta: Usuario[],
+        public comentarios: Comentario[]
+    ){}
+}
